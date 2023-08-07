4c80b1539082e83b4fd8035b60e630b7d243fb61
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-07 23:35:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 
-def getResponse(town):
+def get_response(town):
   payload = {'n':'','M':'','T':'','q':'', 'lang':'ru'}
-  response = requests.get('https://wttr.in/'+ town, params=payload)
+  response = requests.get(f'https://wttr.in/{town}', params=payload)
   response.raise_for_status()
   print(response.text)
 
 def main():
   cities = ['Шереметьево','Лондон','Череповец']
   for city in cities:
-    getResponse(city)
+    get_response(city)
   
 if __name__ == '__main__':
   main()
